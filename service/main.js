6c665ac7d6a7b3992ae0d0c4c432294cd6764009
marru1423 <claritasentoy23@gmail.com>
2023-05-09 16:37:27 +0800

    hotdog

---
+++ service/main.js
@@ -0,0 +1,20 @@
+function toggleNav(elementID) {
+    const openNav = document.getElementById(elementID)
+    
+      openNav.dataset.shown = openNav.dataset.shown === "false"
+          ? showElement(openNav) // will return "true"
+          : hideElement(openNav) // will return "false"
+    
+    }
+    function showElement(element) {
+      element.classList.add('block')
+      element.classList.remove('hidden')
+      return "true"
+    }
+    
+    function hideElement(element) {
+      element.classList.add('hidden')
+      element.classList.remove('block')
+      return "false"
+    }
+    console.log(toggleNav)
