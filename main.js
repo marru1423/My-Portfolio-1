db416eeb20508d6ae647450d624aa7d9ef807cbf
marru1423 <claritasentoy23@gmail.com>
2023-07-17 02:40:45 +0800

    s

--- main.js
+++ main.js
@@ -1,44 +1,52 @@
 function toggleNav(elementID) {
-    const openNav = document.getElementById(elementID)
-    
-      openNav.dataset.shown = openNav.dataset.shown === "false"
-          ? showElement(openNav) // will return "true"
-          : hideElement(openNav) // will return "false"
-    
-    }
-    function showElement(element) {
-      element.classList.add('block')
-      element.classList.remove('hidden')
-      return "true"
-    }
-    
-    function hideElement(element) {
-      element.classList.add('hidden')
-      element.classList.remove('block')
-      return "false"
-    }
-    console.log('Hello World')
+  const openNav = document.getElementById(elementID)
 
-      const scr = document.getElementById('scrl')
+  openNav.dataset.shown = openNav.dataset.shown === "false"
+    ? showElement(openNav) // will return "true"
+    : hideElement(openNav) // will return "false"
 
-    function myScrlup() {
-      scr.classList.add('scrlups')
-    }
-    function myScrlup() {
-      scr.classList.add('ssss')
-    }
-
-    let searchBox = document.querySelector('#box-search');
-    console.log(searchBox)
-    let searchBtn = document.querySelector('.search-fab');
-    console.log(searchBtn)
-
-    searchBtn.onclick = function () {
-    const url = `https://www.google.com/search?q=${searchBox.value}`;
-    window.open(url, '_blank')
+}
+function showElement(element) {
+  element.classList.add('block')
+  element.classList.remove('hidden')
+  return "true"
 }
 
-// const Name = "marru";
-// const Age = 28;
-// let String = `My name ${Name} And my Age ${Age}`;
-// console.log(String);
+function hideElement(element) {
+  element.classList.add('hidden')
+  element.classList.remove('block')
+  return "false"
+}
+console.log('Hello World')
+
+const scr = document.getElementById('scrl')
+
+function myScrlup() {
+  scr.classList.add('scrlups')
+}
+function myScrlup() {
+  scr.classList.add('ssss')
+}
+
+let searchBox = document.querySelector('#box-search');
+console.log(searchBox)
+let searchBtn = document.querySelector('.search-fab');
+console.log(searchBtn)
+
+searchBtn.onclick = function () {
+  const url = `https://www.google.com/search?q=${searchBox.value}`;
+  window.open(url, '_blank')
+}
+
+// let showClick = document.getElementById('buttons')
+
+// function openClicks() {
+//   if (showClick.classList.contains('animoff')) {
+//     showClick.classList.add('anim')
+//     showClick.classList.remove('animoff')
+//   } else {
+//     showClick.classList.add('animoff')
+//     showClick.classList.remove('anim')
+//   }
+// }
+// console.log(showClick)
