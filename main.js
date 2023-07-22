e297a56ba124412e006ad63ec856a7fd6b7ffe1f
marru1423 <claritasentoy23@gmail.com>
2023-07-22 17:49:17 +0800

    asdsad

--- main.js
+++ main.js
@@ -1,37 +1,19 @@
-function toggleNav(elementID) {
-  const openNav = document.getElementById(elementID)
-
-  openNav.dataset.shown = openNav.dataset.shown === "false"
-    ? showElement(openNav) // will return "true"
-    : hideElement(openNav) // will return "false"
-
-}
-function showElement(element) {
-  element.classList.add('block')
-  element.classList.remove('hidden')
-  return "true"
-}
-
-function hideElement(element) {
-  element.classList.add('hidden')
-  element.classList.remove('block')
-  return "false"
-}
-console.log('Hello World')
 
 // let closeAllApps = document.getElementById('Homes')
 // let services = document.getElementById('Services')
+const openNav = document.getElementById('myHeader')
 
-// function toggleNav(){
-//   if(closeAllApps.classList.contains('hidden')){
-//     closeAllApps.classList.add('block')
-//     closeAllApps.classList.remove('hidden')
+function toggleNav(){
+  if(openNav.classList.contains('hidden')){
+    openNav.classList.add('block')
+    openNav.classList.remove('hidden')
 
-//   }else{
-//     closeAllApps.classList.add('hidden')
-//     closeAllApps.classList.remove('block')
-//   }
-// }
+  }else{
+    openNav.classList.add('hidden')
+    openNav.classList.remove('block')
+  }
+}
+console.log(openNav)
 
 const scr = document.getElementById('scrl')
 
